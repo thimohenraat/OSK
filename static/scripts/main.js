60a108c8cd1f99f27ea4b289f6d1f75f4ca3bb5e
Thimo <thimohenraat@gmail.com>
2024-12-06 13:21:37 +0100

    styiling

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,33 +1,34 @@
 import { handleSearchFormSubmit } from './search.js';
 import { sortByDate } from './sort.js';
 import { renderResults } from './render.js';
-import { getCurrentResults, setCurrentResults  } from './state.js';
+import { getCurrentResults, setCurrentResults } from './state.js';
 import { handleIndexingFormSubmit, checkIndexStatus } from './index.js';
 import { renderFileTree } from './fileTree.js';
 
-document.getElementById("search-form").addEventListener("submit", (e) => {
-    handleSearchFormSubmit(e, (results, fileStructure) => {
-        setCurrentResults(results);
-        renderResults(results);
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("search-form").addEventListener("submit", (e) => {
+        handleSearchFormSubmit(e, (results, fileStructure) => {
+            setCurrentResults(results);
+            renderResults(results);
 
-        const matchingFiles = results.map(result => result.path);
+            const matchingFiles = results.map(result => result.path);
 
-        renderFileTree(fileStructure, matchingFiles);  // Geef ook matchingFiles door
-        
+            renderFileTree(fileStructure, matchingFiles);  // Geef ook matchingFiles door
+        });
     });
-});
 
-document.getElementById("sort-button").addEventListener("click", () => {
-    const sortedResults = sortByDate(getCurrentResults());
-    renderResults(sortedResults);
-});
-
-// Indexeerfunctionaliteit
-document.getElementById("index-form").addEventListener("submit", (e) => {
-    handleIndexingFormSubmit(e, (data) => {
-        console.log("Indexering voltooid:", data);
+    document.getElementById("sort-button").addEventListener("click", () => {
+        const sortedResults = sortByDate(getCurrentResults());
+        renderResults(sortedResults);
     });
-});
 
-// Roep de functie aan om de indexstatus te controleren
-checkIndexStatus();
+    // Indexeerfunctionaliteit
+    document.getElementById("index-form").addEventListener("submit", (e) => {
+        handleIndexingFormSubmit(e, (data) => {
+            console.log("Indexering voltooid:", data);
+        });
+    });
+
+    // Roep de functie aan om de indexstatus te controleren
+    checkIndexStatus();
+});
